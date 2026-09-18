a0225267728d3cc1e62c1c185942d251b08da558
enzoanjolimornelas-commits <enzoanjolimornelas@gmail.com>
2026-09-17 22:34:17 -0300

    Completando o basico do MathSpeed

--- js/app.js
+++ js/app.js
@@ -1,22 +1,18 @@
+const formulario = document.getElementById('queijo');
+const level = document.getElementById('nivel');
 const questao = document.getElementById('questao');
 const resposta = document.getElementById('resposta');
-const inserir = document.getElementById('inserir');
-let resultado;
-let verificar = true;
-let alt;
 
-resposta.focus();
-resposta.select();
+let resultado, nivel = 1;
 
-do {
+level.textContent = "Nível " + nivel; 
 
-    let numero1 = Math.ceil(10 * Math.random());
-    let numero2 = Math.ceil(10 * Math.random());
-
-    let sorteioOperacao = Math.ceil(3 * Math.random());
+function gerarNovaQuestao() {
+    let numero1 = Math.ceil(Math.random() * 10);
+    let numero2 = Math.ceil(Math.random() * 10);
     let operacao;
 
-    switch(sorteioOperacao) {
+    switch(Math.ceil(Math.random() * 3)) {
         case 1:
             operacao = '+';
             resultado = numero1 + numero2;
@@ -26,19 +22,33 @@ do {
             resultado = numero1 - numero2;
             break;
         case 3:
-            operacao = 'x';
+            operacao = '*';
             resultado = numero1 * numero2;
+            break;
     }
 
     questao.textContent = `${numero1} ${operacao} ${numero2}`;
-    
-    resposta.addEventListener('click', function(){
-        alt = Number(inserir.value);
-        form.reset();
-    });
+}
 
-    if(Number(inserir.value) !== resultado) {
-        verificar = false;
+formulario.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const respostaDoJogador = Number(resposta.value);
+
+    if(respostaDoJogador === resultado) {
+        console.log("Acertou!");
+        nivel += 1;
+        level.textContent = "Nível " + nivel;
+        gerarNovaQuestao(); 
+    } else {
+        console.log("Errou! Fim de jogo.");
+        gerarNovaQuestao();
+        nivel = 1;
+        level.textContent = "Nível " + nivel;
     }
 
-}while(verificar);
+    formulario.reset();
+    resposta.focus(); 
+});
+
+gerarNovaQuestao();
